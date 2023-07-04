a6a2756dfeb58a11749a548cc7bda4d8edac0308
Asesh Shrestha <aseshshres@gmail.com>
2023-07-04 18:27:41 +0545

    Redis connection works properly

--- server.js
+++ server.js
@@ -3,33 +3,54 @@ const express = require('express')
 const redis = require('redis')
 
 const app = express();
-const REDIS_PORT = 'redis://redis'
+const REDIS_PORT = 'redis://redis-container:6379'
 
-const client = redis.createClient();
+const client = redis.createClient({
+	url: 'redis://redis:6379'
+});
 
 client.connect().then(() => {
     console.log('Connected to Redis');
 }).catch((err) => {
-    console.log(err.message);
+    console.log('Error connecting to Redis: ', err.message);
 })
 
 client.on('connect', () => { console.log(`Redis is connected on port ${REDIS_PORT}`);});
 
-client.on('error', (error) => { console.log('Redis error' , error)});
+client.on('error', (error) => { console.log('Redis connection error' , error)});
 
-function getRedisData() {
-  client.get('counter', async (err, cache_data) => {
-    if(cache_data) {
-      return res.status(200).send({count: JSON.parse(cache_data)});
-    } else {
-      return res.status(200).send({error: 'Key doesnt exists'});
-    }
-  });
+async function getRedisData(res) {
+	let key = 'counter';
+	let value = await client.get(key);
+	if(value) {
+		value++;
+		client.set(key,value);
+		res.send(`Hello ${key}, ${value}!`);
+		console.log(`${key}, ${value}`);
+	}
+	else {
+		value = 1;
+		client.set(key,value);
+		res.send(`Hello ${key}, ${value}!`);
+		console.log(`${key}, ${value}`);
+	}
+	
+//  await client.get('counter', async (err, cache_data) => {
+//		console.log('Retrieving the counter data from Redis');
+//    if(cache_data) {
+//			console.log('counter data found in Redis');
+//      return res.status(200).send({count: JSON.parse(cache_data)});
+//    } else {
+//			console.log('counter data NOT found in Redis');
+//      return res.status(200).send({error: 'Key doesnt exists'});
+//    }
+//  });
 }
 
 app.get('/api/v1/redis', (req, res) => {
+	console.log('Received get request for endpoint: /api/v1/redis');
   try {
-    getRedisData();
+    return getRedisData(res);
   }
   catch(error) {
     console.log(error);
